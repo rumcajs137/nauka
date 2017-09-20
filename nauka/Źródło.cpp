a7fd325c5fd190ec57b3a6c41db487047480c51d
Filip Kłos <filipklos@hotmail.com>
2017-09-20 23:31:49 +0200

    ok

--- nauka/Źródło.cpp
+++ nauka/Źródło.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main()
 {
 	int x = 10;
-	int y = 20;
+	int y = 5;
 	cout << x + y << endl;
 	cout << x - y << endl;
+	cout << x*y << endl;
+	cout << x / y << endl;
+	cout << x%y << endl;
+	cout << x*y + x / y << endl;
+	cout << (x*y) + (x / y)+(x/y) << endl;
 	system("pause");
 
 }
