29606e64f565f4b9dd01a8f144c92bc4670c3256
Filip Kłos <filipklos@hotmail.com>
2017-11-14 20:41:22 +0100

    poprawki

---
+++ KolkoKrzyzyk2/KolkoKrzyzyk/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace KolkoKrzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pole1 = new System.Windows.Forms.PictureBox();
+            this.pole2 = new System.Windows.Forms.PictureBox();
+            this.pole3 = new System.Windows.Forms.PictureBox();
+            this.pole4 = new System.Windows.Forms.PictureBox();
+            this.pole5 = new System.Windows.Forms.PictureBox();
+            this.pole6 = new System.Windows.Forms.PictureBox();
+            this.pole7 = new System.Windows.Forms.PictureBox();
+            this.pole8 = new System.Windows.Forms.PictureBox();
+            this.pole9 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tura = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pole1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pole1
+            // 
+            this.pole1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole1.Location = new System.Drawing.Point(48, 46);
+            this.pole1.Margin = new System.Windows.Forms.Padding(0);
+            this.pole1.Name = "pole1";
+            this.pole1.Size = new System.Drawing.Size(100, 100);
+            this.pole1.TabIndex = 0;
+            this.pole1.TabStop = false;
+            this.pole1.WaitOnLoad = true;
+            this.pole1.Click += new System.EventHandler(this.pole1_Click);
+            // 
+            // pole2
+            // 
+            this.pole2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole2.Location = new System.Drawing.Point(148, 46);
+            this.pole2.Margin = new System.Windows.Forms.Padding(0);
+            this.pole2.Name = "pole2";
+            this.pole2.Size = new System.Drawing.Size(100, 100);
+            this.pole2.TabIndex = 1;
+            this.pole2.TabStop = false;
+            this.pole2.WaitOnLoad = true;
+            this.pole2.Click += new System.EventHandler(this.pole2_Click);
+            // 
+            // pole3
+            // 
+            this.pole3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole3.Location = new System.Drawing.Point(248, 46);
+            this.pole3.Margin = new System.Windows.Forms.Padding(0);
+            this.pole3.Name = "pole3";
+            this.pole3.Size = new System.Drawing.Size(100, 100);
+            this.pole3.TabIndex = 2;
+            this.pole3.TabStop = false;
+            this.pole3.WaitOnLoad = true;
+            this.pole3.Click += new System.EventHandler(this.pole3_Click);
+            // 
+            // pole4
+            // 
+            this.pole4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole4.Location = new System.Drawing.Point(48, 146);
+            this.pole4.Margin = new System.Windows.Forms.Padding(0);
+            this.pole4.Name = "pole4";
+            this.pole4.Size = new System.Drawing.Size(100, 100);
+            this.pole4.TabIndex = 3;
+            this.pole4.TabStop = false;
+            this.pole4.WaitOnLoad = true;
+            this.pole4.Click += new System.EventHandler(this.pole4_Click);
+            // 
+            // pole5
+            // 
+            this.pole5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole5.Location = new System.Drawing.Point(148, 146);
+            this.pole5.Margin = new System.Windows.Forms.Padding(0);
+            this.pole5.Name = "pole5";
+            this.pole5.Size = new System.Drawing.Size(100, 100);
+            this.pole5.TabIndex = 4;
+            this.pole5.TabStop = false;
+            this.pole5.WaitOnLoad = true;
+            this.pole5.Click += new System.EventHandler(this.pole5_Click);
+            // 
+            // pole6
+            // 
+            this.pole6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole6.Location = new System.Drawing.Point(248, 146);
+            this.pole6.Margin = new System.Windows.Forms.Padding(0);
+            this.pole6.Name = "pole6";
+            this.pole6.Size = new System.Drawing.Size(100, 100);
+            this.pole6.TabIndex = 5;
+            this.pole6.TabStop = false;
+            this.pole6.WaitOnLoad = true;
+            this.pole6.Click += new System.EventHandler(this.pole6_Click);
+            // 
+            // pole7
+            // 
+            this.pole7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole7.Location = new System.Drawing.Point(48, 246);
+            this.pole7.Margin = new System.Windows.Forms.Padding(0);
+            this.pole7.Name = "pole7";
+            this.pole7.Size = new System.Drawing.Size(100, 100);
+            this.pole7.TabIndex = 6;
+            this.pole7.TabStop = false;
+            this.pole7.WaitOnLoad = true;
+            this.pole7.Click += new System.EventHandler(this.pole7_Click);
+            // 
+            // pole8
+            // 
+            this.pole8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole8.Location = new System.Drawing.Point(148, 246);
+            this.pole8.Margin = new System.Windows.Forms.Padding(0);
+            this.pole8.Name = "pole8";
+            this.pole8.Size = new System.Drawing.Size(100, 100);
+            this.pole8.TabIndex = 7;
+            this.pole8.TabStop = false;
+            this.pole8.WaitOnLoad = true;
+            this.pole8.Click += new System.EventHandler(this.pole8_Click);
+            // 
+            // pole9
+            // 
+            this.pole9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pole9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole9.Location = new System.Drawing.Point(248, 246);
+            this.pole9.Margin = new System.Windows.Forms.Padding(0);
+            this.pole9.Name = "pole9";
+            this.pole9.Size = new System.Drawing.Size(100, 100);
+            this.pole9.TabIndex = 8;
+            this.pole9.TabStop = false;
+            this.pole9.WaitOnLoad = true;
+            this.pole9.Click += new System.EventHandler(this.pole9_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(392, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 92);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "OD NOWA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tura
+            // 
+            this.tura.Location = new System.Drawing.Point(501, 75);
+            this.tura.Name = "tura";
+            this.tura.Size = new System.Drawing.Size(30, 30);
+            this.tura.TabIndex = 10;
+            this.tura.TabStop = false;
+            this.tura.WaitOnLoad = true;
+            this.tura.Click += new System.EventHandler(this.tura_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(417, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 22);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "KOLEJ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(667, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tura);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pole9);
+            this.Controls.Add(this.pole8);
+            this.Controls.Add(this.pole7);
+            this.Controls.Add(this.pole6);
+            this.Controls.Add(this.pole5);
+            this.Controls.Add(this.pole4);
+            this.Controls.Add(this.pole3);
+            this.Controls.Add(this.pole2);
+            this.Controls.Add(this.pole1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pole1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tura)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pole1;
+        private System.Windows.Forms.PictureBox pole2;
+        private System.Windows.Forms.PictureBox pole3;
+        private System.Windows.Forms.PictureBox pole4;
+        private System.Windows.Forms.PictureBox pole5;
+        private System.Windows.Forms.PictureBox pole6;
+        private System.Windows.Forms.PictureBox pole7;
+        private System.Windows.Forms.PictureBox pole8;
+        private System.Windows.Forms.PictureBox pole9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox tura;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KolkoKrzyzyk2/KolkoKrzyzyk/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace KolkoKrzyzyk
+{
+    public partial class Form1 : Form
+    {
+        char p1, p2, p3;
+        char p4, p5, p6;
+        char p7, p8, p9;
+        char kto;
+
+        void sprawdz()
+        {
+            string w;
+            if (p1 == p2 && p2 == p3 && p1 != 'n' ||
+                 p4 == p5 && p5 == p6 && p4 != 'n' ||
+                 p7 == p8 && p8 == p9 && p7 != 'n' ||
+                 p1 == p4 && p4 == p7 && p1 != 'n' ||
+                 p2 == p5 && p5 == p8 && p2 != 'n' ||
+                 p3 == p6 && p6 == p9 && p3 != 'n' ||
+                 p1 == p5 && p5 == p9 && p1 != 'n' ||
+                 p3 == p5 && p5 == p7 && p3 != 'n')
+            {
+                if (kto == 'x')
+                {
+                    w = "Wygrywa koteł_1!";
+                }
+                else
+                {
+                    w = "Wygrywa koteł_2!";
+                }
+                SoundPlayer simpleSound = new SoundPlayer("img/heil.wav");
+                simpleSound.Play();
+                DialogResult dialog = MessageBox.Show(w, "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (dialog == DialogResult.OK)
+                    Application.Restart();
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            pole1.ImageLocation = "img/n.bmp";
+            pole2.ImageLocation = "img/n.bmp";
+            pole3.ImageLocation = "img/n.bmp";
+            pole4.ImageLocation = "img/n.bmp";
+            pole5.ImageLocation = "img/n.bmp";
+            pole6.ImageLocation = "img/n.bmp";
+            pole7.ImageLocation = "img/n.bmp";
+            pole8.ImageLocation = "img/n.bmp";
+            pole9.ImageLocation = "img/n.bmp";
+            tura.ImageLocation = "img/smallo.bmp";
+
+            p1 = 'n'; p2 = 'n'; p3 = 'n';
+            p4 = 'n'; p5 = 'n'; p6 = 'n';
+            p7 = 'n'; p8 = 'n'; p9 = 'n';
+
+            kto = 'o';
+
+            pole1.Enabled = true;
+            pole2.Enabled = true;
+            pole3.Enabled = true;
+            pole4.Enabled = true;
+            pole5.Enabled = true;
+            pole6.Enabled = true;
+            pole7.Enabled = true;
+            pole8.Enabled = true;
+            pole9.Enabled = true;
+        }
+        
+        private void pole2_Click(object sender, EventArgs e)
+        {
+            if (p2 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole2.ImageLocation = "img/o.bmp";
+                    p2 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole2.ImageLocation = "img/x.bmp";
+                    p2 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole2.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole3_Click(object sender, EventArgs e)
+        {
+            if (p3 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole3.ImageLocation = "img/o.bmp";
+                    p3 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole3.ImageLocation = "img/x.bmp";
+                    p3 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole3.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole4_Click(object sender, EventArgs e)
+        {
+            if (p4 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole4.ImageLocation = "img/o.bmp";
+                    p4 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole4.ImageLocation = "img/x.bmp";
+                    p4 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole4.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole5_Click(object sender, EventArgs e)
+        {
+            if (p5 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole5.ImageLocation = "img/o.bmp";
+                    p5 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole5.ImageLocation = "img/x.bmp";
+                    p5 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole5.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole6_Click(object sender, EventArgs e)
+        {
+            if (p6 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole6.ImageLocation = "img/o.bmp";
+                    p6 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole6.ImageLocation = "img/x.bmp";
+                    p6 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole6.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole7_Click(object sender, EventArgs e)
+        {
+            if (p7 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole7.ImageLocation = "img/o.bmp";
+                    p7 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole7.ImageLocation = "img/x.bmp";
+                    p7 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole7.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole8_Click(object sender, EventArgs e)
+        {
+            if (p8 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole8.ImageLocation = "img/o.bmp";
+                    p8 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole8.ImageLocation = "img/x.bmp";
+                    p8 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole8.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole9_Click(object sender, EventArgs e)
+        {
+            if (p9 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole9.ImageLocation = "img/o.bmp";
+                    p9 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole9.ImageLocation = "img/x.bmp";
+                    p9 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole9.Enabled = false;
+                sprawdz();
+            }
+        }
+
+        private void pole1_Click(object sender, EventArgs e)
+        {
+            if (p1 == 'n')
+            {
+                if (kto == 'o')
+                {
+                    pole1.ImageLocation = "img/o.bmp";
+                    p1 = 'o';
+                    kto = 'x';
+                    tura.ImageLocation = "img/smallx.bmp";
+
+
+                }
+                else
+                {
+                    pole1.ImageLocation = "img/x.bmp";
+                    p1 = 'x';
+                    kto = 'o';
+                    tura.ImageLocation = "img/smallo.bmp";
+                }
+                pole1.Enabled = false;
+                sprawdz();
+            }
+
+
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+
+
+        private void tura_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
